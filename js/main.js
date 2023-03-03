a3dff2c8222d4c3ee149b286d31d968a1987e6e3
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-03 14:48:24 +0100

    grid-structure

---
+++ js/main.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/*
+----------
+Functions
+----------
+*/
+
+function createCell(element1, element2) {
+    const cell = document.createElement(element1);
+    cell.classList.add(element2);
+    return cell;
+};
+
+function numberValue(item, value) {
+    return item.innerText = value
+};
+
+function appentToContainer(container, cell) {
+    return container.append(cell);
+};
+
+function createEasyStructure(container) {
+    container.innerHTML = '';
+    container.classList.replace('d-none', 'd-flex');
+
+    const e = document.getElementById("difficult-select");
+    let value = e.value;
+
+    if (value === 'Easy') {
+        for (let i = 1; i <= 100; i++) {
+            const createdItem = createCell('div', 'cell');
+            const container = document.getElementById('container');
+            appentToContainer(container, createdItem);
+            numberValue(createdItem, i);
+        };
+    };
+
+    if (value === 'Medium') {
+        for (let i = 1; i <= 81; i++) {
+            const createdItem = createCell('div', 'cell-medium');
+            const container = document.getElementById('container');
+            appentToContainer(container, createdItem);
+            numberValue(createdItem, i);
+        };
+    };
+
+    if (value === 'Hard') {
+        for (let i = 1; i <= 49; i++) {
+            const createdItem = createCell('div', 'cell-hard');
+            const container = document.getElementById('container');
+            appentToContainer(container, createdItem);
+            numberValue(createdItem, i);
+        };
+    };
+    return;
+};
+
+/*
+---------
+Main
+---------
+*/
+
+const button = document.querySelector('.start-button');
+
+button.addEventListener('click', function () {
+    const container = document.getElementById('container');
+    createEasyStructure(container);
+
+    const cell = document.querySelectorAll('.cell,.cell-medium,.cell-hard');
+
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('click', function () {
+            cell[i].classList.add('clicked-cell');
+            console.log(numberValue(cell, i + 1));
+            // let arrayClickedCell = [];
+            // arrayClickedCell.push(numberValue(cell, i + 1));
+            // console.log(arrayClickedCell);
+        });
+    };
+});
